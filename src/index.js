20c3592c900483c66a10fbb00a565a5cbec3cf6b
Renat <suleymanov_renat@mail.ru>
2021-01-22 12:03:16 +0300

    Правка ошибок линтера

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import personage from './js/Personage';
-import functionForPersonage from './js/functionForPers';
+import Team from './js/Set';
+import Character from './js/Character';
+import Bowman from './js/Bowman';
 
-functionForPersonage(personage, ['name', 'level']);
+const Bow = new Bowman('Bow');
+const Swordsman = new Character('Swo', 'Swordsman');
+const TeamBowAndSwo = new Team();
+TeamBowAndSwo.addAll(Bow, Swordsman);
