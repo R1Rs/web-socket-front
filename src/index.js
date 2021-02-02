3e82b59961b38ba43718b92f5ec159917a98e736
Renat <suleymanov_renat@mail.ru>
2021-02-02 22:09:48 +0300

    Math

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Team from './js/Set';
-import Character from './js/Character';
-import Bowman from './js/Bowman';
-
-const Bow = new Bowman('Bow');
-const Swordsman = new Character('Swo', 'Swordsman');
-const TeamBowAndSwo = new Team();
-TeamBowAndSwo.addAll(Bow, Swordsman);
