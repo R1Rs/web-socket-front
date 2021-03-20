e9ce88d247435ac2d7fc28300462e633b5d6f173
Renat <suleymanov_renat@mail.ru>
2021-03-20 20:30:28 +0300

    infr

--- src/js/Daemon.js
+++
@@ -1,9 +0,0 @@
-import MagicPeople from './MagicPeople';
-
-export default class Daemon extends MagicPeople {
-  constructor(name) {
-    super(name, 'Daemon');
-    this.basicAttack = 25;
-    this.defence = 25;
-  }
-}
--- src/js/Magician.js
+++
@@ -1,9 +0,0 @@
-import MagicPeople from './MagicPeople';
-
-export default class Magician extends MagicPeople {
-  constructor(name) {
-    super(name, 'Magician');
-    this.basicAttack = 10;
-    this.defence = 25;
-  }
-}
