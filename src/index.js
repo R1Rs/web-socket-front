6102d457d0edeeb7778bdf87b60327f7b2e02466
Renat <suleymanov_renat@mail.ru>
2021-03-23 01:43:40 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-named-as-default
+import GamePlay from './js/GamePlay';
+import './css/style.css';
+
+new GamePlay().init(document.querySelector('.container'));
