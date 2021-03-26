445469713be258c0eaee8799107280ad6c1771c9
Renat <suleymanov_renat@mail.ru>
2021-03-27 01:01:32 +0300

    Task 1

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -9,9 +9,19 @@ export default class GamePlay {
   init(container) {
     this.container = container;
     this.container.innerHTML = `
+    <div> 
         <div class="board-container">
           <div data-id="board" class="board"></div>
         </div>
+        <h2>Счёт
+          <p> Попадания: 
+            <span class="hits"></span>
+          </p>
+          <p> Промахи:
+            <span class="misses"></span>
+          </p>
+        </h2>
+    </div>
     `;
     this.board = document.querySelector('.board');
 
@@ -22,6 +32,8 @@ export default class GamePlay {
     }
 
     this.changePositionGoblin();
+    this.listenerCellMouseMoved();
+    this.listenerCellClick();
   }
 
   changePositionGoblin() {
@@ -30,8 +42,48 @@ export default class GamePlay {
       const activePosition = position.next().value;
       this.board.children.forEach((cell) => {
         cell.classList.remove("cell__active");
+        cell.classList.remove("bam");
       });
       this.board.children[activePosition].classList.add("cell__active");
-    }, 1000);
+    }, 5000);
+  }
+
+  listenerCellMouseMoved() {
+    this.board.children.forEach((cell) => {
+      cell.addEventListener("mouseover", (el) => {
+        // if (cell.classList.contains("cell__active")) {
+        const hammer = document.createElement("div");
+        hammer.classList.add("cursor__active");
+        hammer.style.left = `${el.clientX - 100}px`;
+        hammer.style.top = `${el.clientY - 70}px`;
+        cell.insertAdjacentElement("beforebegin", hammer);
+      });
+      cell.addEventListener("mouseout", () => {
+        const hammer = document.querySelectorAll(".cursor__active");
+        hammer.forEach((elem) => elem.remove());
+      });
+    });
+  }
+
+  listenerCellClick() {
+    this.board.children.forEach((cell) => {
+      cell.addEventListener("click", (el) => {
+        if (cell.classList.contains("cell__active")) {
+          cell.classList.remove("cell__active");
+          cell.classList.add("bam");
+          let hits = document.querySelector(".hits");
+          // eslint-disable-next-line no-plusplus
+          hits.textContent++;
+        } else {
+          let misses = document.querySelector(".misses");
+          // eslint-disable-next-line no-plusplus
+          misses.textContent++;
+          if (misses.textContent == 2) {
+            alert("GAME OVER");
+            new GamePlay().init(document.querySelector('.container'));
+          }
+        }
+      });
+    });
   }
 }
