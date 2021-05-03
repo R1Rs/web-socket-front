64177827f9ec237f244111faaf67a403fa81cc4c
Renat <suleymanov_renat@mail.ru>
2021-05-03 21:02:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./css/style.css";
+import click from "./js/ClickPopover";
+
+click();
---
+++ src/js/ClickPopover.js
@@ -0,0 +1,6 @@
+export default function click () {
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+       document.querySelector(".popover").classList.toggle("popover__active");
+    })
+}
